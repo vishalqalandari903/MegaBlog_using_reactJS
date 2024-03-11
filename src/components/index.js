239359ef61effd6be43006d9b7d143f2f104b2c5
Vishal Kumar <vishalfreelancing903@gmail.com>
2024-03-11 13:41:19 +0500

    components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "./container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn };
